d99f3092cf6fb362c02778048a8c79384e989054
dottongoto-maker <dottongoto@gmail.com>
2026-04-24 20:55:31 +0300

    Add files via upload

---
+++ java project/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Class: Java Programming");
+        System.out.println("Student Number: 12345");
+    }
+}
